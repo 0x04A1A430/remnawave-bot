9f3602ab49bcccea18ab5aefa65a2d54ba83522f
cy7su <myaaq@yamdex.ru>
2026-09-17 03:51:51 +0400

    fix: attach gift migrations to current head

--- migrations/alembic/versions/9101_add_inline_gift_subscriptions.py
+++ migrations/alembic/versions/9101_add_inline_gift_subscriptions.py
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from alembic import op
 
 revision: str = "9101"
-down_revision: Union[str, None] = "0094"
+down_revision: Union[str, None] = "0124"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
